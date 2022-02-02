4f4f93ba90501a9ae741907ea88e79d630838962
rob49157 <robertopaipo@hotmail.com>
2022-02-01 19:45:09 -0500

    typedefs

--- Develop/server/schemas/typeDefs.js
+++ Develop/server/schemas/typeDefs.js
@@ -1,18 +1,38 @@
 const { gql }=require('apollo-server-express')
 
 const typeDefs= gql`
-type Book{
+type me{
     _id:ID
-    name: String
+    User:String
+    
 
 }
-type Query{
+
+
+type User{
+    _id=ID
+    username:Sting
+    email= String
+    BookCount= Number
+    savedBooks= [book]
+}
+ Book{
+    bookid=ID
+    authors=[]
+    title=String
+    image=[Image!]
+    link=[Link!]
+
+
+ }
+ type Query{
     books:[Book]!
     book(bookid: ID!): Book
 }
-type Mutation{
+ type Mutation{
     addbook(name:String!): Book
     removebook(bookid: ID!):Book
 }`
+
 
 module.exports= typeDefs
